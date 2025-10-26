1a1358862b9a3baf1eb510fecd57c0ebd5b725ed
Victoria <127861886+VittaSmith@users.noreply.github.com>
2025-10-26 17:19:20 +0300

    Исправление ошибки

--- Lesson_2/Main.java
+++ Lesson_2/Main.java
@@ -115,9 +115,9 @@ public class Main {
 
     public static void arrayChange(){
         int[] arr = {0,0,1,1,0,1,0,1};
-        for (int mas : arr){
-            if (mas==1) mas=0;
-            else mas = 1;
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == 1) arr[i] = 0;
+            else arr[i] = 1;
         }
         System.out.println(Arrays.toString(arr));
     }
